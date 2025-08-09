eea124d34a360bc068e9a85c99d8efcc23efd746
Eloy Rengel <eloypanita@gmail.com>
2025-08-09 12:54:38 -0400

    añadiendo login (solo frontend) de desktop y tablet

--- src/componentes/login.jsx
+++ src/componentes/login.jsx
@@ -15,8 +15,35 @@ function Login() {
 
   return (
     <>
+        {!isMobile && (
+          <div className="row m-2 pantalla_desktop">
+
+            <div className="col-lg-6  text-white p-0  titulo_fondo_negro titulo_desktop" >
+                <h1>
+                <img src={Logo} alt="" />
+              <span translate="no"> GYM</span>  
+            </h1>
+            <h1 translate="no" className="h1Abajo">STRIKE</h1>
+            </div>
+
+            
+               <form action="" className="col-lg-6  text-white p-0 login login-Desktop">
+                        <div className="datos ">
+                            <p>INGRESE SU CEDULA</p>
+                                <input type="number" />
+                        </div>
+                        <div className="datos">
+                            <p>INGRESE SU CLAVE </p>
+                            <input type="password" /> 
+                        </div>
+                        <button>Ingresar</button>
+                    </form>
+           
+            
+          </div>  
+        )}
+
          {isMobile && (
-        <div className="">
          <div className="pantalla_mobile">
 
             <div className="titulo_fondo_negro">
@@ -41,8 +68,7 @@ function Login() {
                     </form>
             </div>
          </div>
-
-        </div>
+       
          )}
      
     </>
